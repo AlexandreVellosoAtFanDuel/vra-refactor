439057daa125550666df692f01b63fbd75709f6d
Alexandre Velloso <alexandre.velloso@fanduel.com>
2025-08-27 17:32:09 +0100

    Implement DateUtils

--- src/main/java/com/betfair/video/api/domain/utils/DateUtils.java
+++ src/main/java/com/betfair/video/api/domain/utils/DateUtils.java
@@ -1,13 +1,22 @@
 package com.betfair.video.api.domain.utils;
 
+import java.util.Calendar;
 import java.util.Date;
 
 public class DateUtils {
-    public static Date shiftDateByField(Date itemStartDate, int second, int i) {
-        return null;
+    public static Date shiftDateByField(Date date, int field, int amount) {
+        Calendar calendar = getCalendarInstance(date);
+        calendar.add(field, amount);
+        return new Date(calendar.getTimeInMillis());
+    }
+
+    private static Calendar getCalendarInstance(Date date) {
+        Calendar c = Calendar.getInstance();
+        c.setTime(date);
+        return c;
     }
 
     public static Date getCurrentDate() {
-        return null;
+        return new Date();
     }
 }
