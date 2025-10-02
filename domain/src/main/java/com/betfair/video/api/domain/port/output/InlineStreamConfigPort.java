8ac6d6e8321a091a7988ba9c642133492ba24dc3
Alexandre Velloso <alexandre.velloso@fanduel.com>
2025-10-02 16:13:42 +0100

    Implement logic for inline stream config

--- domain/src/main/java/com/betfair/video/api/domain/port/output/InlineStreamConfigPort.java
+++ domain/src/main/java/com/betfair/video/api/domain/port/output/InlineStreamConfigPort.java
@@ -1,9 +1,5 @@
 package com.betfair.video.api.domain.port.output;
 
 public interface InlineStreamConfigPort {
-
-
-    boolean isArchivedProviderInList(Integer integer, boolean b);
-
     boolean isProviderInList(Integer integer, Integer integer1);
 }
--- output/src/main/java/com/betfair/video/api/output/adapter/InlineStreamConfigAdapter.java
+++ output/src/main/java/com/betfair/video/api/output/adapter/InlineStreamConfigAdapter.java
@@ -1,21 +1,35 @@
 package com.betfair.video.api.output.adapter;
 
+import com.betfair.video.api.domain.dto.entity.TypeChannel;
 import com.betfair.video.api.domain.port.output.InlineStreamConfigPort;
+import com.betfair.video.api.output.util.ConfigFormatter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import java.util.Map;
+import java.util.Set;
 
 @Component
 public class InlineStreamConfigAdapter implements InlineStreamConfigPort {
 
-    @Override
-    public boolean isArchivedProviderInList(Integer integer, boolean b) {
-        // TODO: fetch from configuration
-        return false;
-    }
+    @Value("${stream.inline.mobile}")
+    private String streamInlineMobile;
+
+    private Map<Integer, Set<Integer>> streamInlineMap = null;
 
     @Override
-    public boolean isProviderInList(Integer integer, Integer integer1) {
-        // TODO: fetch from configuration
-        return false;
+    public boolean isProviderInList(Integer providerId, Integer channelTypeId) {
+        if (streamInlineMap == null) {
+            streamInlineMap = buildStreamInlineMap(streamInlineMobile);
+        }
+
+        return streamInlineMap.containsKey(channelTypeId) && streamInlineMap.get(channelTypeId).contains(providerId);
+    }
+
+    private Map<Integer, Set<Integer>> buildStreamInlineMap(String mobile) {
+        Set<Integer> mobileSet = ConfigFormatter.csvToIntegerSet(mobile);
+
+        return Map.of(TypeChannel.MOBILE.getId(), mobileSet);
     }
 
 }
